a08559d73996571ca2e9f4bb23822ec2cf785a56
João Senna Radicchi <joaoradicchi@gmail.com>
2023-10-24 20:03:17 -0600

    create most of the card handlers and middleware

--- back-end/src/router.ts
+++ back-end/src/router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { createNewCard, deleteCard, getAllCards } from "./handlers/card";
+import { body, validationResult } from "express-validator";
+import { validationErrorMiddleware } from "./middleware/validationError";
 
 const router = Router();
 
 // CARDS
-router.get("/card", (req, res) => {
-  res.json({ message: "cards here" });
-});
+router.get('/card', getAllCards);
+router.post('/card', body('name').isString(), body('imageUrl').optional().isString(), validationErrorMiddleware, createNewCard);
+router.delete('/card/:id', body('id').isString(), validationErrorMiddleware,deleteCard);
 
 export default router;
